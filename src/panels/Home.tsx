dc1cc0dd0dc72812fb84de0fd1e75e31f02a79a2
rurkk <367414@niuitmo.ru>
2024-09-18 08:32:42 +0300

    random dog

--- src/panels/Home.tsx
+++ src/panels/Home.tsx
@@ -10,7 +10,7 @@ import {
   Avatar,
   NavIdProps,
 } from '@vkontakte/vkui';
-import { UserInfo } from '@vkontakte/vk-bridge';
+import bridge, { UserInfo } from '@vkontakte/vk-bridge';
 import { useRouteNavigator } from '@vkontakte/vk-mini-apps-router';
 
 export interface HomeProps extends NavIdProps {
@@ -20,6 +20,35 @@ export interface HomeProps extends NavIdProps {
 export const Home: FC<HomeProps> = ({ id, fetchedUser }) => {
   const { photo_200, city, first_name, last_name } = { ...fetchedUser };
   const routeNavigator = useRouteNavigator();
+
+  const getRandomImage = async () => {
+    const response = await fetch('https://dog.ceo/api/breeds/image/random');
+    const data = await response.json();
+    return data.message;
+  };
+
+  const Story = async () => {
+    const imageUrl = await getRandomImage();
+
+    bridge.send('VKWebAppShowStoryBox', {
+      background_type: 'image',
+      url: imageUrl,
+      attachment: {
+        text: `more`,
+        type: 'photo',
+        owner_id: 743784474,
+        id: 12345678
+      }
+    })
+    .then((data) => {
+      if (data.result) {
+        console.log(data); // Редактор историй открыт
+      }
+    })
+    .catch((error) => {
+      console.log(error); // Ошибка
+    });
+  };
 
   return (
     <Panel id={id}>
@@ -39,6 +68,16 @@ export const Home: FC<HomeProps> = ({ id, fetchedUser }) => {
           </Button>
         </Div>
       </Group>
+
+      <Group header={<Header mode="secondary">Редактор историй ВКонтакте</Header>}>
+        <Div>
+          <Button stretched size="l" mode="secondary" onClick={Story}>
+            Открыть редактор историй с шобакой
+          </Button>
+        </Div>
+      </Group>
     </Panel>
   );
 };
+
+export default Home;
